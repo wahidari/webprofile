e99d1fa1bce9ba1620a2429fc20726d022086650
wahidari <wahiid.ari@gmail.com>
2022-02-26 19:50:50 +0700

    Update data.js

--- pages/api/product/data.js
+++ pages/api/product/data.js
@@ -5,7 +5,7 @@ export const products = [
         "name": "Product A",
         "category": "Makanan",
         "price": "10000",
-        "image": "https://images.unsplash.com/photo-1518843875459-f738682238a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dmVnZXRhYmxlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+        "image": "https://images.unsplash.com/photo-1518843875459-f738682238a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dmVnZXRhYmxlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=492&height=364&q=60",
         "phone": "628123456789",
         "seller": "Seller A",
         "description": "Lorem Ipsum is simply dummy text of the printing"
@@ -16,7 +16,7 @@ export const products = [
         "name": "Product B",
         "category": "Pertanian",
         "price": "20000",
-        "image": "https://images.unsplash.com/photo-1597362925123-77861d3fbac7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dmVnZXRhYmxlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+        "image": "https://images.unsplash.com/photo-1597362925123-77861d3fbac7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dmVnZXRhYmxlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=492&height=364&q=60",
         "phone": "628123456789",
         "seller": "Seller B",
         "description": "Lorem Ipsum is simply dummy text of the printing"
@@ -27,7 +27,7 @@ export const products = [
         "name": "Product C",
         "category": "Makanan",
         "price": "30000",
-        "image": "https://images.unsplash.com/photo-1522184216316-3c25379f9760?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHZlZ2V0YWJsZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        "image": "https://images.unsplash.com/photo-1522184216316-3c25379f9760?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHZlZ2V0YWJsZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=492&height=364&q=60",
         "phone": "628123456789",
         "seller": "Seller C",
         "description": "Lorem Ipsum is simply dummy text of the printing"
@@ -38,7 +38,7 @@ export const products = [
         "name": "Product D",
         "category": "Minuman",
         "price": "25000",
-        "image": "https://images.unsplash.com/photo-1601648764658-cf37e8c89b70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fHZlZ2V0YWJsZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        "image": "https://images.unsplash.com/photo-1601648764658-cf37e8c89b70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fHZlZ2V0YWJsZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=492&height=364&q=60",
         "phone": "628123456789",
         "seller": "Seller D",
         "description": "Lorem Ipsum is simply dummy text of the printing"
@@ -49,7 +49,7 @@ export const products = [
         "name": "Product E",
         "category": "Kesehatan",
         "price": "15000",
-        "image": "https://images.unsplash.com/photo-1619566636858-adf3ef46400b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGZydWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        "image": "https://images.unsplash.com/photo-1619566636858-adf3ef46400b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGZydWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=492&height=364&q=60",
         "phone": "628123456789",
         "seller": "Seller E",
         "description": "Lorem Ipsum is simply dummy text of the printing"
@@ -60,7 +60,7 @@ export const products = [
         "name": "Product F",
         "category": "Peralatan",
         "price": "40000",
-        "image": "https://images.unsplash.com/photo-1439127989242-c3749a012eac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDd8fGZydWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        "image": "https://images.unsplash.com/photo-1439127989242-c3749a012eac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDd8fGZydWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=492&height=364&q=60",
         "phone": "628123456789",
         "seller": "Seller F",
         "description": "Lorem Ipsum is simply dummy text of the printing"
@@ -71,7 +71,7 @@ export const products = [
         "name": "Product G",
         "category": "Pakaian",
         "price": "50000",
-        "image": "https://images.unsplash.com/photo-1597714026720-8f74c62310ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzF8fGZydWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        "image": "https://images.unsplash.com/photo-1597714026720-8f74c62310ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzF8fGZydWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=492&height=364&q=60",
         "phone": "628123456789",
         "seller": "Seller G",
         "description": "Lorem Ipsum is simply dummy text of the printing"
