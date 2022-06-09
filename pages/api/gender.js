eb566fa9586597dd36747ba6e53566fafd2e4fcb
wahidari <wahiid.ari@gmail.com>
2022-06-09 11:22:19 +0700

    Update gender.js

--- pages/api/gender.js
+++ pages/api/gender.js
@@ -1,10 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const gender = [
-    { "id": 1, "name": "Laki Laki", "total": "1000" }, 
-    { "id": 2, "name": "Perempuan", "total": "800" }
+	{ "id": 1, "name": "Laki Laki", "total": "1000" },
+	{ "id": 2, "name": "Perempuan", "total": "800" }
 ];
 
 export default function allHandler(req, res) {
-    res.status(200).json(gender);
+	res.status(200).json(gender);
 };
 
