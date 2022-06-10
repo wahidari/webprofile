94acdc2b241dc9144acca6be8f3d6a676bdd785f
wahidari <wahiid.ari@gmail.com>
2022-06-10 12:22:52 +0700

    Update data.js

--- pages/api/product/data.js
+++ pages/api/product/data.js
@@ -1,79 +1,79 @@
 export const products = [
-    {
-        "id": 1,
-        "slug": "product-a",
-        "name": "Product A",
-        "category": "Makanan",
-        "price": "10000",
-        "image": "https://images.unsplash.com/photo-1518843875459-f738682238a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dmVnZXRhYmxlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=492&height=364&q=60",
-        "phone": "628123456789",
-        "bumdes": "BUM Desa A",
-        "description": "Lorem Ipsum is simply dummy text of the printing"
-    },
-    {
-        "id": 2,
-        "slug": "product-b",
-        "name": "Product B",
-        "category": "Pertanian",
-        "price": "20000",
-        "image": "https://images.unsplash.com/photo-1597362925123-77861d3fbac7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dmVnZXRhYmxlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=492&height=364&q=60",
-        "phone": "628123456789",
-        "bumdes": "BUM Desa B",
-        "description": "Lorem Ipsum is simply dummy text of the printing"
-    },
-    {
-        "id": 3,
-        "slug": "product-c",
-        "name": "Product C",
-        "category": "Makanan",
-        "price": "30000",
-        "image": "https://images.unsplash.com/photo-1522184216316-3c25379f9760?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHZlZ2V0YWJsZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=492&height=364&q=60",
-        "phone": "628123456789",
-        "bumdes": "BUM Desa C",
-        "description": "Lorem Ipsum is simply dummy text of the printing"
-    },
-    {
-        "id": 4,
-        "slug": "product-d",
-        "name": "Product D",
-        "category": "Minuman",
-        "price": "25000",
-        "image": "https://images.unsplash.com/photo-1601648764658-cf37e8c89b70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fHZlZ2V0YWJsZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=492&height=364&q=60",
-        "phone": "628123456789",
-        "bumdes": "BUM Desa D",
-        "description": "Lorem Ipsum is simply dummy text of the printing"
-    },
-    {
-        "id": 5,
-        "slug": "product-e",
-        "name": "Product E",
-        "category": "Kesehatan",
-        "price": "15000",
-        "image": "https://images.unsplash.com/photo-1619566636858-adf3ef46400b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGZydWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=492&height=364&q=60",
-        "phone": "628123456789",
-        "bumdes": "BUM Desa E",
-        "description": "Lorem Ipsum is simply dummy text of the printing"
-    },
-    {
-        "id": 6,
-        "slug": "product-f",
-        "name": "Product F",
-        "category": "Peralatan",
-        "price": "40000",
-        "image": "https://images.unsplash.com/photo-1439127989242-c3749a012eac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDd8fGZydWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=492&height=364&q=60",
-        "phone": "628123456789",
-        "bumdes": "BUM Desa F",
-        "description": "Lorem Ipsum is simply dummy text of the printing"
-    },
-    {
-        "id": 7,
-        "slug": "product-g",
-        "name": "Product G",
-        "category": "Pakaian",
-        "price": "50000",
-        "image": "https://images.unsplash.com/photo-1597714026720-8f74c62310ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzF8fGZydWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=492&height=364&q=60",
-        "phone": "628123456789",
-        "bumdes": "BUM Desa G",
-        "description": "Lorem Ipsum is simply dummy text of the printing"
-    }
+	{
+		"id": 1,
+		"slug": "product-a",
+		"name": "Product A",
+		"category": "Makanan",
+		"price": "10000",
+		"image": "https://images.unsplash.com/photo-1518843875459-f738682238a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dmVnZXRhYmxlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=492&height=364&q=60",
+		"phone": "628123456789",
+		"bumdes": "BUM Desa A",
+		"description": "Lorem Ipsum is simply dummy text of the printing"
+	},
+	{
+		"id": 2,
+		"slug": "product-b",
+		"name": "Product B",
+		"category": "Pertanian",
+		"price": "20000",
+		"image": "https://images.unsplash.com/photo-1597362925123-77861d3fbac7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dmVnZXRhYmxlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=492&height=364&q=60",
+		"phone": "628123456789",
+		"bumdes": "BUM Desa B",
+		"description": "Lorem Ipsum is simply dummy text of the printing"
+	},
+	{
+		"id": 3,
+		"slug": "product-c",
+		"name": "Product C",
+		"category": "Makanan",
+		"price": "30000",
+		"image": "https://images.unsplash.com/photo-1522184216316-3c25379f9760?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHZlZ2V0YWJsZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=492&height=364&q=60",
+		"phone": "628123456789",
+		"bumdes": "BUM Desa C",
+		"description": "Lorem Ipsum is simply dummy text of the printing"
+	},
+	{
+		"id": 4,
+		"slug": "product-d",
+		"name": "Product D",
+		"category": "Minuman",
+		"price": "25000",
+		"image": "https://images.unsplash.com/photo-1601648764658-cf37e8c89b70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fHZlZ2V0YWJsZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=492&height=364&q=60",
+		"phone": "628123456789",
+		"bumdes": "BUM Desa D",
+		"description": "Lorem Ipsum is simply dummy text of the printing"
+	},
+	{
+		"id": 5,
+		"slug": "product-e",
+		"name": "Product E",
+		"category": "Kesehatan",
+		"price": "15000",
+		"image": "https://images.unsplash.com/photo-1619566636858-adf3ef46400b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGZydWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=492&height=364&q=60",
+		"phone": "628123456789",
+		"bumdes": "BUM Desa E",
+		"description": "Lorem Ipsum is simply dummy text of the printing"
+	},
+	{
+		"id": 6,
+		"slug": "product-f",
+		"name": "Product F",
+		"category": "Peralatan",
+		"price": "40000",
+		"image": "https://images.unsplash.com/photo-1439127989242-c3749a012eac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDd8fGZydWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=492&height=364&q=60",
+		"phone": "628123456789",
+		"bumdes": "BUM Desa F",
+		"description": "Lorem Ipsum is simply dummy text of the printing"
+	},
+	{
+		"id": 7,
+		"slug": "product-g",
+		"name": "Product G",
+		"category": "Pakaian",
+		"price": "50000",
+		"image": "https://images.unsplash.com/photo-1597714026720-8f74c62310ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzF8fGZydWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=492&height=364&q=60",
+		"phone": "628123456789",
+		"bumdes": "BUM Desa G",
+		"description": "Lorem Ipsum is simply dummy text of the printing"
+	}
 ]
