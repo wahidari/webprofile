1d7bc1b61d3323c16278717f8558c41b26119b38
wahidari <wahiid.ari@gmail.com>
2022-06-25 02:11:06 +0700

    Update ProductCard.js

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,14 +1,22 @@
 import Image from "next/image"
-import { FaWhatsapp } from "react-icons/fa"
+import Link from "next/link";
 
-export default function ProductCard({ id, slug, name, category, price, image, phone, seller, description }) {
-
+export default function ProductCard({ id, umkm, name, price, image, description, link }) {
 	return (
 		<>
 			<style jsx>
 				{`
+					.card-link {
+						color: #212529;
+						text-decoration: none !important;
+						font-weight: 500;
+					}
+					.card-link:hover {
+						color: #0d6efd;
+						transition-duration: 0.3s;
+					}
 					.shadow-custom {
-						box-shadow: 0 2px 8px rgb(0 0 0 / 10%);
+						box-shadow: 0 1px 8px rgb(0 0 0 / 10%);
 					}
 				`}
 			</style>
@@ -16,18 +24,21 @@ export default function ProductCard({ id, slug, name, category, price, image, ph
 			<div className="card card-img-hover-zoom h-100 shadow-custom border-0 rounded-3">
 				<Image
 					alt={name}
-					src={image}
+					src={image?.photo ? image.photo : process.env.API_ROUTE + "/media/produk.png"}
 					width={270}
 					height={200}
 					quality={90}
 					className="img-fluid rounded-top"
 				/>
 				<div className="card-body">
-					<h5 className="fw-medium">{name}</h5>
+					<Link href={`/umkm/${umkm}/produk/${id}`}>
+						<a className="text-decoration-none"><h5 className="card-link lh-sm mb-2">{name}</h5></a>
+					</Link>
+					<h6 className="fw-medium">Rp. {price}</h6>
 					<p className="mb-0 fs-15 text-secondary">{description}</p>
 				</div>
 				<div className="card-footer bg-white">
-					<a href={`https://wa.me/${phone}?text=Saya%20ingin%20memesan%20${name}`} className="btn btn-sm btn-success" rel="noreferrer" target="_blank"><i className="me-2"><FaWhatsapp /></i>Whatsapp</a>
+					<a href={link} className="btn btn-sm btn-success" rel="noreferrer" target="_blank">Link Pembelian</a>
 				</div>
 			</div>
 		</>
